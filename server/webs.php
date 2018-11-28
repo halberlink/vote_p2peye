b7d6b0aa0fde73e87c23c51fe5b032b141be928d
liujinqiang <liujinqiang@p2peye.com>
2018-11-28 23:45:16 +0800

    修改

--- server/webs.php
+++ server/webs.php
@@ -1,5 +1,6 @@
 <?php  
 
+include 'mysql.php';
 
 $type = $_GET['type'];
 
@@ -12,12 +13,67 @@ if($type == 'begin'){
 	$status = $redis->get("poll_status");
 
 	if($status == 2){
-		echo json_encode(array("code"=>"200","data"=>$status,"message"=>"success"));exit;
+
+		$res = conntentMysql("select * from peoples where status ='0'");
+
+		$res = current($res);
+
+		$res = conntentMysql("UPDATE peoples SET status='1' where name = '".$res['name']."'");
+
+		echo json_encode(array("code"=>"200","data"=>$res,"message"=>"success"));exit;
 	}else{
 		echo json_encode(array("code"=>"400","data"=>$status,"message"=>"felid"));exit;
 	}
 		
 }
 
+if($type == 'next'){
+
+	//明天继续写
+
+	$res_now = conntentMysql("select * from peoples where status ='1'");
+
+	$res_now_name = $res_now[0]['name'];
+
+	$res = conntentMysql("UPDATE peoples SET status='2' where name = '".$res_now_name."'");
+
+
+	$res_will = conntentMysql("select * from peoples where status ='0'");
+
+	$res_will = current($res_will);
+
+	$res_will_name = $res_will['name'];
+
+	$res = conntentMysql("UPDATE peoples SET status='1' where name = '".$res_will_name."'");
+
+	if($res){
+		$res = conntentMysql("select * from peoples where status ='1'");
+	}
+
+	echo json_encode(array("code"=>"200","data"=>$res,"message"=>"success"));exit;	
+
+}
+
+if($type == 'webs'){
+
+	$redis = new Redis();
+	$redis->connect('127.0.0.1', 16379);
+
+	$status = $redis->get("poll_status");
+
+
+	$res = conntentMysql("select * from peoples where status ='1'");
+
+	
+
+	if(empty($res) || $status == 1){
+		echo json_encode(array("code"=>"4030","data"=>$res,"message"=>"活动未开始"));exit;
+
+	}
+
+	echo json_encode(array("code"=>"200","data"=>$res,"message"=>"success"));exit;	
+
+
+}
 
 ?>
