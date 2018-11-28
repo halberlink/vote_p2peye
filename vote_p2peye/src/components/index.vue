ac1b3289a8d8d5b621766f309e0806b9572eec5c
hb Haobo <bo.hao@chetuobang.com>
2018-11-28 22:35:50 +0800

    init vue

---
+++ vote_p2peye/src/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <span @click="jumpTo('/InformationEntry')">PC展示开始投票</span>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods:{
+      jumpTo:function(url){
+        this.$router.push(url)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  span{
+    cursor: pointer;
+  }
+</style>
