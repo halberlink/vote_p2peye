d9dd24b078eecbbdb762263fd47556408161f73d
hb Haobo <bo.hao@chetuobang.com>
2018-12-06 15:21:33 +0800

    优化提示xinxi

--- vote_p2peye/src/components/index_m.vue
+++ vote_p2peye/src/components/index_m.vue
@@ -12,7 +12,7 @@
      <mt-button @click="clear" size="large" type="primary">清除登录缓存</mt-button>
    </div>
    <div class="des">*请输入姓名作为评选人的标识，非评选人请勿输入！</div>
-   <div class="des">*整个评分过程中请不要进行返回操作如果发生连接失败 刷新页面即可 谢谢配合！</div>
+   <div class="des">*为了保证投票流程顺利进行，整个评分过程中请不要进行返回操作如果发生连接失败 刷新页面即可 谢谢配合！</div>
 
   </div>
 
